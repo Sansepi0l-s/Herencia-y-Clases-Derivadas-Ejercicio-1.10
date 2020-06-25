db3b61b64028a2ad158c5b2bfe0cdf5ffa90d55e
Heymmar Zeledon <eliazithzeledon27@gmail.com>
2020-06-25 01:30:17 -0600

    Add files via upload
    Se requiere diseñar una clase para llevar el control de los empleados de una empresa que reciben comisión por sus ventas el cual contiene los siguientes datos: nombre, número de seguro, monto de ventas brutas, tarifa de comisión. A demás se requiere que la clase implemente el método ToString que permita obtener todos los datos del empleado como una cadena.

---
+++ Empleado por comision.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_10_ejercicio_1
+{
+    public class Empleado_por_comision
+    {
+        private string nombre;
+        private string numeroSeguroSocial;
+        private decimal ventasBrutas;
+        private decimal tarifaComision;
+
+        public Empleado_por_comision(string nombre, string nss, decimal ventas, decimal tarifas)
+        {
+            Nombre = nombre;
+            NumeroSeguroSocial = nss;
+            VentasBrutas = ventas;
+            TarifaComision = tarifas;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string NumeroSeguroSocial
+        {
+            get { return numeroSeguroSocial; }
+            set { numeroSeguroSocial = value; }
+        }
+
+        public decimal VentasBrutas
+        {
+            set { ventasBrutas = value; }
+            get { return ventasBrutas; }
+           
+        }
+
+        public decimal TarifaComision
+        {
+            set { tarifaComision = value; }
+            get { return tarifaComision; }
+           
+        }
+
+        public decimal Ingresos() { return (TarifaComision * VentasBrutas); }
+
+        public override string ToString()
+        {
+            return string.Format("Empleado: {0} \nNumero de seguro: {1}\n" + "Ventas brutas: {2:F2}\nTarifa de comision: {3:F2}\n" +
+            "Ingresos: {4:F2}", Nombre, NumeroSeguroSocial,
+                VentasBrutas, TarifaComision, Ingresos());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_10_ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado_por_comision e = new Empleado_por_comision("Karol Argeñal", "16-73775-2", 5000.0M, .04M);
+            // muestra en pantalla los datos del empleado por comisión con salario base
+            Console.WriteLine(e.ToString());
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
